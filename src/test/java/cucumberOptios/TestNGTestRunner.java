1ec085a1d303f4ef4a52d565dd69bce5a11d84bc
helenwu <helen.ly.wu@outlook.com>
2024-09-02 09:16:07 +1000

    Add listener

--- src/test/java/cucumberOptios/TestNGTestRunner.java
+++ src/test/java/cucumberOptios/TestNGTestRunner.java
@@ -3,7 +3,7 @@ package cucumberOptios;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
-
+@Listeners(TestListener.class)
 @CucumberOptions(features="src/test/java/features",glue= "stepDefinitions",monochrome=true,tags="@PlaceOrder or s@OffersPage",plugin={"pretty","html:target/cucumber.html","json:target/cucumber.json",
 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","rerun:target/failed_scenarios.txt"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
