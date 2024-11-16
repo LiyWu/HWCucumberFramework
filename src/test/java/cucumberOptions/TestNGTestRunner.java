e6c6b8ac5c404d32e989d40352c486a38110b277
LiyWu <helen.ly.wu@outlook.com>
2024-11-17 08:47:57 +1100

    CloseableHttpClient httpClient

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -2,7 +2,10 @@ package cucumberOptions;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import listener.TestListener;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+
 @Listeners(TestListener.class)
 @CucumberOptions(features="src/test/java/features",glue= "stepDefinitions",monochrome=true,tags="@PlaceOrder or s@OffersPage",plugin={"pretty","html:target/cucumber.html","json:target/cucumber.json",
 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","rerun:target/failed_scenarios.txt"})
--- src/test/java/listener/TestListener.java
+++ src/test/java/listener/TestListener.java
@@ -1,3 +1,4 @@
+package listener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
